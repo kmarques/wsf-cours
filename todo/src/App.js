9e5fed95859b123cd839dcaf913015b0e7b0cc64
Karl MARQUES <kmarques@vetixy.com>
2020-03-30 14:20:53 +0200

    [REACT] with context

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,31 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import Board from "./components/Board";
+import BoardContext from "./context/board";
 
 function App() {
-  const [boards, setBoards] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  // ComponentDidMount
-  useEffect(
-    function() {
-      console.log("App mounted");
-      
-      fetch("http://localhost:3004/boards")
-      .then(response => response.json())
-      .then(data => {
-        setLoading(false);
-        setBoards(data)
-      });
-    },
-    []
-  );
-
   return (
     <div>
-      {
-        !loading && boards.map(board => <Board {...board}/>)
-      }
-      {loading && <p>Loading...</p>}
+    <BoardContext.Consumer>
+      {({boards, loading}) => {
+        return <>
+        {!loading && boards.map(board => <Board key={board.id} {...board}/>)}
+        {loading && <p>Loading...</p>}
+        </>
+      }}
+    </BoardContext.Consumer>  
     </div>
   );
 }
